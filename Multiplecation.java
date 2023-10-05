631bcb2289ad13ff50e108d43f10eba2b6d0b325
ashish-sangavkar <ashish.sangavkar47@gmail.com>
2023-10-05 10:43:59 +0530

    multiply commit 1

---
+++ Multiplecation.java
@@ -0,0 +1,8 @@
+public class Multiplecation {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        System.out.println("Multiplecation is : "+(a*b));
+    }
+}
